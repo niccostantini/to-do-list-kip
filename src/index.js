e575eb8602cd5f4e2e3fc6f58bee2bd76cf46545
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-08-20 14:55:02 +0200

    Style sidebar

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ const addProjectButton = document.querySelector('.addProjectButton');
 const addTaskButton = document.querySelector('.addTaskButton');
 const addButtons = document.querySelectorAll("#addButton, .addProjectButton, .addTaskButton")
 
+// Add listeners for add button graphics
+// Show AddProject and AddTask upon hover an PLUS button and keep them showw
+// also when hovering on the task and project button themselves
 addButtons.forEach(button => button.addEventListener("mouseover", () => {
-
     addProjectButton.classList.add("showAddProject");
     addTaskButton.classList.add("showAddTask");
-
 }))
 
+// Hide AddTask and AddProject on mouseout
 addButtons.forEach(button => button.addEventListener("mouseout", () => {
     addProjectButton.classList.remove("showAddProject");
     addTaskButton.classList.remove("showAddTask");
