8b2f13f52cae7630fb6401dffe630cde8ad8c5d8
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-08-20 13:19:33 +0200

    Style and script add buttons

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import "./styles/normalize.css";
 import "./styles/styles.css";
 
 
+const addButton = document.querySelector('#addButton');
+const addProjectButton = document.querySelector('.addProjectButton');
+const addTaskButton = document.querySelector('.addTaskButton');
+const addButtons = document.querySelectorAll("#addButton, .addProjectButton, .addTaskButton")
+
+addButtons.forEach(button => button.addEventListener("mouseover", () => {
+
+    addProjectButton.classList.add("showAddProject");
+    addTaskButton.classList.add("showAddTask");
+
+}))
+
+addButtons.forEach(button => button.addEventListener("mouseout", () => {
+    addProjectButton.classList.remove("showAddProject");
+    addTaskButton.classList.remove("showAddTask");
+}))
+
